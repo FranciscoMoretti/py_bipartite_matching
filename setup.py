1be89d32a7ee1781405096fe76bd4772c872f363
Francisco Moretti <franciscoemoretti@gmail.com>
2021-02-14 20:40:06 -0300

    Drop support for python 3.6 to use latest tox

--- setup.py
+++ setup.py
@@ -19,14 +19,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Francisco Moretti",
     author_email='franciscoemoretti@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
