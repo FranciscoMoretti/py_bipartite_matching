d81b79e7da2b52df4e28e4b1fcdc93c56a2e32dc
Francisco Moretti <franciscoemoretti@gmail.com>
2021-02-25 21:35:51 -0300

    modify example to make it bipartite-able

--- tests/test_kosaraju.py
+++ tests/test_kosaraju.py
@@ -12,11 +12,12 @@ from py_bipartite_matching.matching.kosaraju import Kosaraju
 # @given(bipartite_graph())
 def test_kosaraju():
     # Create a graph given in the above diagram 
-    g = Kosaraju(5) 
-    g.addEdge(1, 0) 
-    g.addEdge(0, 2) 
-    g.addEdge(2, 1) 
-    g.addEdge(0, 3) 
+    g = Kosaraju(5)
+    g.addEdge(1, 2) 
+    g.addEdge(0, 1) 
+    g.addEdge(2, 3) 
+    g.addEdge(3, 0) 
+    g.addEdge(3, 2) 
     g.addEdge(3, 4) 
     scc = g.getSCC() 
-    assert scc == {frozenset({3}), frozenset({4}), frozenset({0, 1, 2})}
+    assert scc == {frozenset({4}), frozenset({0, 1, 2, 3})}
