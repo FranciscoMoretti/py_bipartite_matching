8729e1c540923c94997d55f494ac4d58d48c82b5
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-14 19:15:08 -0300

    unit test for networkx's hopcroft_karp

--- tests/test_hopcroft_karp.py
+++ tests/test_hopcroft_karp.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 from py_bipartite_matching.matching.hopcroft_karp import HopcroftKarp
+import networkx as nx 
 
 
 class TestHopcroftKarp:
@@ -45,3 +46,92 @@ class TestHopcroftKarp:
         matchings, maximum_matching = hk.get_maximum_matching_num()
         assert maximum_matching == expected
         assert matchings == 7
+
+
+class TestHopcroftKarpNetworkx:
+    """
+    Testing the implementation of the Hopcroft Karp algorithm.
+    """
+    def test_hopcroft_karp(self):
+
+        G = nx.Graph()
+        G.add_edge(0, "v0")
+        G.add_edge(0, "v1")
+        G.add_edge(1, "v0")
+        G.add_edge(1, "v4")
+        G.add_edge(2, "v2")
+        G.add_edge(2, "v3")
+        G.add_edge(3, "v0")
+        G.add_edge(3, "v4")
+        G.add_edge(4, "v0")
+        G.add_edge(4, "v3")
+
+        matching = nx.bipartite.hopcroft_karp_matching(G)
+        matching = {k: v for (k, v) in matching.items() if isinstance(k, int)}
+        expected: Dict[int, str] = {0: "v1", 1: "v4", 2: "v2", 3: "v0", 4: "v3"}
+
+        assert matching == expected
+        assert len(matching) == 5
+
+        G = nx.Graph()
+        G.add_edge('A', 1)
+        G.add_edge('A', 2)
+        G.add_edge('B', 2)
+        G.add_edge('B', 3)
+        G.add_edge('C', 2)
+        G.add_edge('D', 3)
+        G.add_edge('D', 4)
+        G.add_edge('D', 5)
+        G.add_edge('D', 6)
+        G.add_edge('E', 4)
+        G.add_edge('E', 7)
+        G.add_edge('F', 7)
+        G.add_edge('G', 7)
+
+        expected1: Dict[str, int] = {'A': 1, 'B': 3, 'C': 2, 'D': 5, 'E': 4, 'F': 7}
+        expected2: Dict[str, int] = {'A': 1, 'B': 3, 'C': 2, 'D': 5, 'E': 4, 'G': 7}
+        matching = nx.bipartite.hopcroft_karp_matching(G)
+        matching = {k: v for (k, v) in matching.items() if isinstance(k, str)}
+        assert matching == expected1 or matching == expected2
+        assert len(matching) == 6
+
+        G = nx.Graph()
+        G.add_edge(1, "a")
+        G.add_edge(1, "c")
+        G.add_edge(2, "a")
+        G.add_edge(2, "c")
+        G.add_edge(3, "c")
+        G.add_edge(3, "b")
+        G.add_edge(4, "e")
+
+        expected: Dict[int, str] = {1: 'a', 2: 'c', 3: 'b', 4: 'e'}
+        matching = nx.bipartite.hopcroft_karp_matching(G, {1, 2, 3, 4})
+        matching = {k: v for (k, v) in matching.items() if isinstance(k, int)}
+        assert matching == expected
+        assert len(matching) == 4
+
+        G = nx.Graph()
+        # G.add_edge('A', 3)
+        G.add_edge('A', 4)
+        # G.add_edge('B', 3)
+        G.add_edge('B', 4)
+        G.add_edge('C', 3)
+        G.add_edge('D', 1)
+        G.add_edge('D', 5)
+        G.add_edge('D', 7)
+        # G.add_edge('E', 1)
+        G.add_edge('E', 2)
+        G.add_edge('E', 7)
+        # G.add_edge('F', 2)
+        G.add_edge('F', 8)
+        G.add_edge('G', 6)
+        G.add_edge('H', 2)
+        G.add_edge('H', 4)
+        G.add_edge('H', 8)
+
+        expected1: Dict[str, int] = {'A': 4,'C': 3, 'D': 1, 'E': 7, 'F': 8, 'G': 6, 'H': 2}
+        expected2: Dict[str, int] = {'B': 4,'C': 3, 'D': 1, 'E': 7, 'F': 8, 'G': 6, 'H': 2}
+        matching = nx.bipartite.hopcroft_karp_matching(G, {'A','B','C','D','E','F','G','H'})
+        matching = {k: v for (k, v) in matching.items() if isinstance(k, str)}
+        assert matching == expected1 or matching == expected2
+        assert len(matching) == 7
