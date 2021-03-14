4d807756df133393df43596b9606a9a90f3fb8d4
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-14 11:16:53 -0300

    Test strongly connected components from networkx

--- tests/test_kosaraju.py
+++ tests/test_kosaraju.py
@@ -8,6 +8,9 @@ import pytest
 
 from py_bipartite_matching.matching.kosaraju import Kosaraju
 from py_bipartite_matching.matching.bipartite import BipartiteGraph
+
+import networkx as nx
+
 # @example(create_cubelet_graph(example_0))
 # @given(bipartite_graph())
 def test_kosaraju():
@@ -20,4 +23,16 @@ def test_kosaraju():
     g.addEdge(3, 2) 
     g.addEdge(3, 4) 
     scc = g.getSCC() 
+    assert scc == {frozenset({4}), frozenset({0, 1, 2, 3})}
+
+def test_scc_networkx():
+    G = nx.DiGraph()
+    G.add_edge(1, 2) 
+    G.add_edge(0, 1) 
+    G.add_edge(2, 3) 
+    G.add_edge(3, 0) 
+    G.add_edge(3, 2) 
+    G.add_edge(3, 4) 
+    scc_generator = nx.strongly_connected_components(G)
+    scc = {frozenset(item) for item in scc_generator}
     assert scc == {frozenset({4}), frozenset({0, 1, 2, 3})}
