19bec1ede921d70545bda91713f2b7eb9089277a
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-24 11:43:42 -0300

    remove nodes from attributes on util function

--- py_bipartite_matching/matching/graphs_utils.py
+++ py_bipartite_matching/matching/graphs_utils.py
@@ -56,7 +56,18 @@ def graph_without_edge(graph, edge_id):
 
 def networkx_graph_without_nodes_of_edge(graph, edge):
     """Returns a copy of this bipartite graph with the given edge and its adjacent nodes removed."""
-    new_graph  = graph.copy()
+    new_graph  = copy.deepcopy(graph)
+
+    if new_graph.graph.get('top') and new_graph.graph.get('bottom'):
+        if edge[0] in new_graph.graph.get('top'):
+            new_graph.graph.get('top').remove(edge[0])
+            new_graph.graph.get('bottom').remove(edge[1])
+        else:
+            new_graph.graph.get('top').remove(edge[1])
+            new_graph.graph.get('bottom').remove(edge[0])
+        assert len(new_graph.graph.get('top')) == len(graph.graph.get('top')) - 1
+        assert len(new_graph.graph.get('bottom')) == len(graph.graph.get('bottom')) - 1
+    
     new_graph.remove_node(edge[0])
     new_graph.remove_node(edge[1])
     return new_graph
