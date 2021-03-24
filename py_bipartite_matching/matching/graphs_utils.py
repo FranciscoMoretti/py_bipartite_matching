eac1e7dbd0043619108177eab7613e7ca24dc0a1
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-24 11:44:22 -0300

    add asserts to improve robustness

--- py_bipartite_matching/matching/graphs_utils.py
+++ py_bipartite_matching/matching/graphs_utils.py
@@ -70,10 +70,15 @@ def networkx_graph_without_nodes_of_edge(graph, edge):
     
     new_graph.remove_node(edge[0])
     new_graph.remove_node(edge[1])
+    assert new_graph != graph
+    assert len(new_graph.nodes) == len(graph.nodes) - 2 
     return new_graph
 
 def networkx_graph_without_edge(graph, edge):
     """Returns a copy of this bipartite graph with the given edge removed."""
     new_graph  = copy.deepcopy(graph)
     new_graph.remove_edge(*edge)
+
+    assert len(new_graph.edges) == len(graph.edges) - 1
+    assert len(new_graph.nodes) == len(graph.nodes)
     return new_graph
