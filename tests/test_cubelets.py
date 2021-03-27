479b45f45c7b4362b886eb78c1fef5df85d064e5
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-27 09:38:10 -0300

    fix test_cubelets unit test

--- tests/test_cubelets.py
+++ tests/test_cubelets.py
@@ -1,49 +1,71 @@
 # -*- coding: utf-8 -*-
-import itertools
-import math
+import networkx as nx
 
-import hypothesis.strategies as st
-from hypothesis import example, given
-import pytest
+from py_bipartite_matching.matching.takeaki_networkx import enum_perfect_matchings_networkx, enum_maximum_matchings_networkx
 
-# from py_bipartite_matching.matching.bipartite import BipartiteGraph, DirectedMatchGraph
-# from py_bipartite_matching.matching.takeaki import enum_perfect_matchings, enum_maximum_matchings
+def print_debug_info(graph, matchings):
+    print(f"Graph and matchings")
+    print(f"Nodes :{graph.nodes}")
+    print(f"Edges :{graph.edges}")
+    print(f"Matchings :")
+    for number, matching in enumerate(matchings):
+        print(f"{number}: {set(matching)}")
+    print("-"*80)
+
+cubelets_matrix = [
+    ["U", "R", "F"],# 0: 0
+    ["D", "F", "R"],# 1: 4
+    ["U", "F", "L"],# 2: 1
+    ["D", "L", "F"],# 3: 5
+    ["U", "L", "B"],# 4: 2
+    ["D", "B", "L"],# 5: 6
+    ["D", "R", "B"],# 6: 7
+    ["U", "B", "R"]]# 7: 3
+
+example_0 = "FLUUFFLB"
+
+def create_cubelet_graph(example):
+    # Create helper functions
+    def ind_to_btm(ind):
+        return ind + 10
+    def btm_to_ind(ind):
+        return ind - 10
+
+    graph =  nx.Graph()
+    size = len(example)
+    
+    top_nodes = list(range(size))
+    bottom_nodes = list(range(ind_to_btm(0), ind_to_btm(size)))
+
+    graph.add_nodes_from(top_nodes, bipartite=0)
+    graph.graph["top"] = top_nodes
+    graph.add_nodes_from(bottom_nodes, bipartite=1)
+    graph.graph["bottom"] = bottom_nodes
+
+    for top_node, element in enumerate(example):
+        for bottom_node in bottom_nodes:
+            if element in cubelets_matrix[btm_to_ind(bottom_node)]:
+                graph.add_edge(top_node, bottom_node)
+
+    return graph
 
 
-# cubelets_matrix = [
-#     ["U", "R", "F"],# 0: 0
-#     ["D", "F", "R"],# 1: 4
-#     ["U", "F", "L"],# 2: 1
-#     ["D", "L", "F"],# 3: 5
-#     ["U", "L", "B"],# 4: 2
-#     ["D", "B", "L"],# 5: 6
-#     ["D", "R", "B"],# 6: 7
-#     ["U", "B", "R"]]# 7: 3
+def test_cubelets_enum_perfect_matchings():
+    print(f"Testing cubelets_enum_perfect_matchings")
 
-# example_0 = "FLUUFFLB"
+    graph = create_cubelet_graph(example_0)
 
-# def create_cubelet_graph(example):
-#     edges = []
-#     for input_index, element in enumerate(example):
-#         for cubelet_index in range(len(cubelets_matrix)):
-#             if element in cubelets_matrix[cubelet_index]:
-#                 edges.append((input_index, cubelet_index))
-#     return BipartiteGraph({e:True for e in edges})       
+    if len(graph.graph['top']) != len(graph.graph['bottom']):
+        pass
 
-# # @example(create_cubelet_graph(example_0))
-# # @given(bipartite_graph())
-# def test_enum_maximum_matchings_correctness():
-#     graph = create_cubelet_graph(example_0)
-#     size = None
-#     matchings = set()
-#     for matching in enum_maximum_matchings(graph):
-#         if size is None:
-#             size = len(matching)
-#         assert len(matching) == size, "Matching has a different size than the first one"
-#         for edge in matching.items():
-#             assert edge in graph, "Matching contains an edge that was not in the graph"
-#         frozen_matching = frozenset(matching.items())
-#         assert frozen_matching not in matchings, "Matching was duplicate"
-#         matchings.add(frozen_matching)
-
+    size = len(graph.graph['top']) # should be equal to graph.right as well
+    matchings = set()
+    for matching in enum_perfect_matchings_networkx(graph):
+        assert len(matching) == size, "Matching has a different size than the first one"
+        for edge in matching.items():
+            assert edge in graph.edges, "Matching contains an edge that was not in the graph"
+        frozen_matching = frozenset(matching.items())
+        assert frozen_matching not in matchings, "Matching was duplicate"
+        matchings.add(frozen_matching)
+    print_debug_info(graph=graph, matchings=matchings)
 
