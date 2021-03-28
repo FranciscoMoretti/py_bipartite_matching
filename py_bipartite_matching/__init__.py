10975b0977c388e59b68d25235790f6c2fc480d8
Francisco Moretti <franciscoemoretti@gmail.com>
2021-03-28 09:06:56 -0300

    Bump version: 0.1.6 → 0.1.7

--- py_bipartite_matching/__init__.py
+++ py_bipartite_matching/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Francisco Moretti"""
 __email__ = 'franciscoemoretti@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 from .takeaki import enum_maximum_matchings, enum_perfect_matchings
