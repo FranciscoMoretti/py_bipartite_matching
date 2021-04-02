a9413bbbca8322a269458af33ead2e9835c5dbd1
Francisco Moretti <franciscoemoretti@gmail.com>
2021-04-02 20:14:15 -0300

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/FranciscoMoretti/py_bipartite_matching',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
