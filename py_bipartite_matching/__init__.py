44c34dfa9d6793171dee5000d275522760bcc6e2
Francisco Moretti <franciscoemoretti@gmail.com>
2021-04-03 19:41:10 -0300

    Bump version: 0.1.13 → 0.1.14

--- py_bipartite_matching/__init__.py
+++ py_bipartite_matching/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Francisco Moretti"""
 __email__ = 'franciscoemoretti@gmail.com'
-__version__ = '0.1.13'
+__version__ = '0.1.14'
 
 from .py_bipartite_matching import enum_maximum_matchings, enum_perfect_matchings
--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/FranciscoMoretti/py_bipartite_matching',
-    version='0.1.13',
+    version='0.1.14',
     zip_safe=False,
 )
