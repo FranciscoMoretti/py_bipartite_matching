26e8c45f178b1dea7102a38d26ece4e580430daf
Francisco Moretti <franciscoemoretti@gmail.com>
2021-04-04 12:11:49 -0300

    Bump version: 0.1.14 → 0.1.15

--- py_bipartite_matching/__init__.py
+++ py_bipartite_matching/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Francisco Moretti"""
 __email__ = 'franciscoemoretti@gmail.com'
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 
 from .py_bipartite_matching import enum_maximum_matchings, enum_perfect_matchings
 from .graphs_utils import top_nodes, bottom_nodes, draw_bipartite, plot_bipartite
