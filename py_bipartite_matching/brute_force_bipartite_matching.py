adb44251165cb15d943183fe67b6d4527df8eb56
Francisco Moretti <franciscoemoretti@gmail.com>
2021-04-25 09:12:57 -0300

    brute force function for perfect matchings

---
+++ py_bipartite_matching/brute_force_bipartite_matching.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Contains classes and functions to enumerate matchings based on brute force methods.
+"""
+from typing import Iterator, Any, Dict
+
+import itertools
+import networkx as nx
+from networkx.algorithms.bipartite.matching import maximum_matching
+
+from .graphs_utils import (top_nodes, bottom_nodes)
+
+__all__ = ['brute_force_enum_perfect_matchings']
+
+
+def brute_force_enum_perfect_matchings(graph: nx.Graph) -> Iterator[Dict[Any, Any]]:
+    if len(list(top_nodes(graph))) != len(list(bottom_nodes(graph))):
+        return
+    size = len(list(top_nodes(graph)))
+    matching = maximum_matching(graph, top_nodes=top_nodes(graph))
+    matching = {k: v for k, v in matching.items() if k in top_nodes(graph)}
+    if matching and len(matching) == size:
+        for values in itertools.product(
+                *map(lambda node: graph.neighbors(node), top_nodes(graph))):
+            if len(set(values)) == len(values):
+                matching = {k: v for k, v in zip(top_nodes(graph), values)}
+                yield matching
